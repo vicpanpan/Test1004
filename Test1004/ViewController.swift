5a863e3061fd2b84b50610bf374bb3667575c0a4
vicpan <pan20051115@gmail.com>
2022-10-04 10:24:40 +0800

    Initial Commit

---
+++ Test1004/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test1004
+//
+//  Created by vicpan on 2022/10/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
